cb0293deb7522730c864e1bec38042ee40322a97
NithyaKuchadi <49386822+NithyaKuchadi@users.noreply.github.com>
2019-12-02 21:46:57 -0800

    updated profile with follower info

--- frontend_combined_withUser/src/components/Profile.js
+++ frontend_combined_withUser/src/components/Profile.js
@@ -6,6 +6,10 @@ import { Modal, Button } from 'react-bootstrap';
 import axios from 'axios';
 import { imageDownload, imageUpload } from '../redux/actions/image_actions';
 import { Redirect } from 'react-router';
+import { isfollowing, follow, unfollow} from '../redux/actions/follow-actions';
+import { Redirect } from 'react-router';
+import { Link } from 'react-router-dom';
+import { followingCount, followerCount } from '../redux/actions/followerfollowing-actions';
 
 class Profile extends Component {
     constructor() {
@@ -29,18 +33,67 @@ class Profile extends Component {
             state: "",
             zipcode: "",
             username: "",
-            IncorrectCredentials:""
+            IncorrectCredentials:"",
+            followDisplay: false,
+            userid:""
         }
 
     }
     async componentDidMount() {
+      var path = this.props.location.pathname;
+        
+        console.log("THE PATH IS IN PROFILE",path );
 
-        let post =
+        var id = path.replace("/profile/", "");
+        console.log("id is ",id);
+
+        this.setState(
+            {
+                userid:id
+            }
+
+        )
+        if (id !== localStorage.getItem('cookie1')) {
+            let data = {
+                "userid": localStorage.getItem('cookie1'),
+                "userid_is_follow": userid
+
+            }
+            await this.props.isfollowing(data);
+            console.log("IN PROFILE this.props.isfollowingvalue", this.props.isfollowingvalue);
+            if (!this.props.isfollowingvalue) {
+                this.setState(
+                    {
+                        followDisplay: true
+                    }
+                )
+            }
+        }
+  let post =
         {
-            "userid": this.props.userid
+            "userid": this.state.userid
         }
         await this.props.profileaction(post);
         if (this.props.profiledetails) {
+
+          await this.props.followingCount(this.props.userid);
+          await this.props.followerCount(this.props.userid);
+
+          if (this.props.follower_no) {
+            this.setState(
+                {
+                    Followers: this.props.follower_no
+                }
+            )
+        }
+
+        if (this.props.following_no) {
+            this.setState(
+               {
+                   Following: this.props.following_no
+                }
+            )
+       }
 
             let date = this.props.profiledetails.dateofbirth;
             if (date) {
@@ -103,10 +156,10 @@ class Profile extends Component {
         let isValid = this.onValidate();
         if (isValid) {
             var data = {};
-            let userid = this.props.userid;
+            let userid = this.state.userid;
          
             if (this.state.ProfileImage) {
-                axios.defaults.withCredentials = true;
+              
                 data = {
                     userID: userid,
                     userhandle: "@" + this.state.username,
@@ -123,7 +176,7 @@ class Profile extends Component {
                 }
             }
             else {
-                axios.defaults.withCredentials = true;
+              
                 data = {
                     userID: userid,
                     userhandle: "@" + this.state.username,
@@ -205,107 +258,203 @@ class Profile extends Component {
         }
     }
     render() {
+      let redirectVar = null;
+      if (!localStorage.getItem('cookie2')) {
+          redirectVar = <Redirect to="/TwitterHome" />;
+      }
+      let profileImageData = <img className="img-style" src="https://img.freepik.com/free-icon/user-filled-person-shape_318-74922.jpg?size=338c&ext=jpg" alt="logo" />
+      if (this.state.ProfileImagePreview) {
+          profileImageData = <img className="img-style" src={this.state.ProfileImagePreview} alt="logo" />
+      }
+      var style = {
+          color: "rgba(29,161,242,1.00)",
+          fontSize: 30,
+          paddingLeft: "10px"
+        };
+      return (
+       
 
-        let redirectVar = null;
-        if (!localStorage.getItem('cookie2')) {
-            redirectVar = <Redirect to="/TwitterHome" />;
-        }
-
-        let profileImageData = <img className="img-style" src="https://img.freepik.com/free-icon/user-filled-person-shape_318-74922.jpg?size=338c&ext=jpg" alt="logo" />
-        if (this.state.ProfileImagePreview) {
-            profileImageData = <img className="img-style" src={this.state.ProfileImagePreview} alt="logo" />
-        }
-
-        return (
-            <div class="col-md-5">
-                {redirectVar}
-                <div>
-                   <div>
-                     <div className="jumbotron345">
-
-                            {profileImageData}
-
-                            <div className="jumbo2">
-                                <div className="para34">
-                                    <br />
-                                   <br/>
-                                    <div className="in">
-                                        <p className="para2">{this.state.userdetails.profilename}</p>
-                                        <button onClick={this.profileedit} className="para6">Edit Profile</button>
-                                    </div><br /><br />
-                                    <p className="para3">{this.state.userdetails.userhandle}</p><br /><br /><br />
-                                    <div className="in">
-                                        <p className="parafirstlastname2"><b>First Name</b>  {this.state.userdetails.firstname} </p>
-                                        <p className="parafirstlastname6"><b>Last Name</b>   {this.state.userdetails.lastname}</p>
-                                    </div><br />
-                                    <p className="parafirstlastname2"><b>Born </b>           {this.state.dateofbirth}</p><br />
-                                    <p className="parafirstlastname2"><b>Joined </b>         {this.state.created_at}</p><br />
-
-                                    <p className="parafirstlastname2">{this.state.Followers} <b>Followers</b></p><br />
-                                    <p className="parafirstlastname2">{this.state.Following} <b>Following</b></p><br />
-
-                                    <p className="parafirstlastname2"> <b>City</b>           {this.state.userdetails.city}</p><br />
-                                    <p className="parafirstlastname2"><b>State</b>           {this.state.userdetails.state} </p><br />
-                                    <p className="parafirstlastname2"><b>Zipcode</b>         {this.state.userdetails.zipcode} </p><br />
-
-                                </div>
-                            </div>
-                            <div>
-                                <Modal className="setModal" show={this.state.show} onHide={this.handleClose} >
-                                <Modal.Header className="item2">
-                                       <Modal.Title >Edit Profile</Modal.Title>
-
-                                 </Modal.Header>
-
-                                    <Modal.Body >
-                                     <h4>User Name</h4>
-                                        <input className="name1" name="username" type="text" defaultValue={this.state.userdetails.username} onChange={this.handleChange}></input>
-                                        <h4>Profile Image</h4>
-                                        <input className="name1" name="ProfileImage" onChange={this.handleChange} type="file" />
-                                        <h4>Bio</h4>
-                                        <input className="name34567" type="text" defaultValue={this.state.userdetails.description}  onChange={this.handleChange} />
-                                        <h4>Website</h4>
-                                        <input className="name1" name="Website" defaultValue={this.state.userdetails.website_url} onChange={this.handleChange} type="text"></input>
-                                        <h4>Birth date</h4>
-                                        <input className="name1" name="dateofbirth" defaultValue={this.state.dateofbirth} onChange={this.handleChange} type="text"></input>
-                                        <h4>City</h4>
-                                        <input className="name1" name="city" defaultValue={this.state.userdetails.city} onChange={this.handleChange} type="text"></input>
-                                        <h4>State</h4>
-                                        <input className="name1" name="state" defaultValue={this.state.userdetails.state} onChange={this.handleChange} type="text"></input>
-                                        <div className="field1234567" style={{ color: "red" }}>{this.state.IncorrectCredentials}</div>
-                                        <h4>Zipcode</h4>
-                                        <input className="name1" name="zipcode" defaultValue={this.state.userdetails.zipcode} onChange={this.handleChange} type="text"></input>
-
-                                    </Modal.Body>
-                                    <Modal.Footer>
-                                        <Button variant="secondary" onClick={this.handleClose}>
-                                            Close
-                                        </Button>
-                                        <Button variant="primary" onClick={this.updatesProfile}>
-                                            Save Changes
-                                        </Button>
-                                    </Modal.Footer>
-                                </Modal>
-                            </div>
-                        </div>
-
-                    </div>
-                   
+          <div class="container-fluid w-87">
+                 {redirectVar}
+              <div className="row">
+              <div class="col-md-3 border">
+    <ul class="list-unstyled" style={{ position: "fixed" }}>
+      <li class="m-4 h6 font-weight-bold">
+        <a>
+          <span style={style}>
+            {" "}
+            <i class="fab fa-twitter"></i>{" "}
+          </span>{" "}
+        </a>
+      </li>
+      <li class="m-4 h6 font-weight-bold">
+        <a onClick={this.homecheck}>
+          <div className="hoveritem">
+            <i class="fas fa-home icon_pad"></i>Home
                 </div>
-            </div>
-        )
-    }
+        </a>
+      </li>
+      <li class="m-4 h6 font-weight-bold">
+        <a onClick={this.explorecheck}>
+          <div className="hoveritem">
+            <i class="fas fa-hashtag icon_pad"></i> Explore
+                </div>
+        </a>
+      </li>
+      <li class="m-4 h6 font-weight-bold">
+        <a onClick={this.notificationcheck}>
+          <div className="hoveritem">
+            <i class="far fa-bell icon_pad"></i>Notifications
+                </div>
+        </a>
+      </li>
+      <li class="m-4 h6 font-weight-bold">
+        <a onClick={this.messagescheck}>
+          <div className="hoveritem">
+            <i class="far fa-envelope icon_pad"></i>Messages
+                </div>
+        </a>
+      </li>
+      <li class="m-4 h6 font-weight-bold">
+        <a onClick={this.bookmarkscheck}>
+          <div className="hoveritem">
+            <i class="far fa-bookmark icon_pad"></i>Bookmarks
+                </div>
+        </a>
+      </li>
+      <li class="m-4 h6 font-weight-bold">
+        <a onClick={this.listcheck}>
+          <div className="hoveritem">
+            <i class="far fa-list-alt icon_pad"></i>Lists
+                </div>
+        </a>
+      </li>
+      <li class="m-4 h6 font-weight-bold">
+        <a onClick={this.profilecheck}>
+          <div className="hoveritem">
+            <i class="fas fa-user-circle icon_pad"></i>Profile
+                </div>
+        </a>
+      </li>
+      <li class="m-4 h6 font-weight-bold">
+        <a>
+          <div className="hoveritem">
+            <i class="far fa-chart-bar icon_pad"></i>Analytics
+                </div>
+        </a>
+      </li>
+      <li class="m-4 h6 font-weight-bold">
+        <button type="button" class="btn btn-primary">
+          Tweet
+              </button>
+      </li>
+    </ul>
+  </div>
+          <div class="col-md-5">
+             <div>
+                  <div>
+                      <div className="jumbotron345">
+
+                          {profileImageData}
+
+                          <div className="jumbo2">
+                              <div className="para34">
+
+                                  <div className="in">
+                                      <p className="para2">{this.state.userdetails.profilename}</p>
+
+                                      <button onClick={this.profileedit} className="para6">Edit Profile</button>
+
+                                      <div className="followtoggle para234"> {this.state.followDisplay && <button onClick={this.onFollow}>Follow</button>}</div>
+
+
+                                  </div>
+                                  <p className="para3">{this.state.userdetails.userhandle}</p><br />
+                                  <div className="in">
+                                      <span className="parafirstlastname2"><b>First Name</b>  {this.state.userdetails.firstname} </span>
+                                      <span className="parafirstlastname6"><b>Last Name</b>   {this.state.userdetails.lastname}</span>
+                                  </div><br />
+                                  <div className="in">
+                                      <span className="parafirstlastname2"><b>Born </b>           {this.state.dateofbirth}</span>
+                                      <span className="parafirstlastname6"><b>Joined </b>         {this.state.created_at}</span>
+                                  </div><br />
+
+                                  <div className="in">
+                                      <p className="increasefont"><Link to="/followers">{this.state.Followers} Followers</Link></p>
+                                      <p className="increasefont parafirstlastname6"><Link to="/followers">{this.state.Following} Following</Link></p>
+                                  </div><br />
+
+                                  <span className="parafirstlastname2"> <b>City</b>           {this.state.userdetails.city}</span>
+                                  <span className="parafirstlastname4"><b>State</b>           {this.state.userdetails.state} </span>
+                                  <span className="parafirstlastname5"><b>Zipcode</b>         {this.state.userdetails.zipcode} </span>
+
+                              </div>
+                          </div>
+                          <div>
+                              <Modal className="setModal" show={this.state.show} onHide={this.handleClose} >
+                                  <Modal.Header className="item2">
+                                      <Modal.Title >Edit Profile</Modal.Title>
+
+                                  </Modal.Header>
+
+                                  <Modal.Body >
+                                      <h4>User Name</h4>
+                                      <input className="name1" name="username" type="text" defaultValue={this.state.userdetails.username} onChange={this.handleChange}></input>
+                                      <h4>Profile Image</h4>
+                                      <input className="name1" name="ProfileImage" onChange={this.handleChange} type="file" />
+                                      <h4>Bio</h4>
+                                      <input className="name34567" type="text" defaultValue={this.state.userdetails.description} onChange={this.handleChange} />
+                                      <h4>Website</h4>
+                                      <input className="name1" name="Website" defaultValue={this.state.userdetails.website_url} onChange={this.handleChange} type="text"></input>
+                                      <h4>Birth date</h4>
+                                      <input className="name1" name="dateofbirth" defaultValue={this.state.dateofbirth} onChange={this.handleChange} type="text"></input>
+                                      <h4>City</h4>
+                                      <input className="name1" name="city" defaultValue={this.state.userdetails.city} onChange={this.handleChange} type="text"></input>
+                                      <h4>State</h4>
+                                      <input className="name1" name="state" defaultValue={this.state.userdetails.state} onChange={this.handleChange} type="text"></input>
+                                      <div className="field1234567" style={{ color: "red" }}>{this.state.IncorrectCredentials}</div>
+                                      <h4>Zipcode</h4>
+                                      <input className="name1" name="zipcode" defaultValue={this.state.userdetails.zipcode} onChange={this.handleChange} type="text"></input>
+
+                                  </Modal.Body>
+                                  <Modal.Footer>
+                                      <Button variant="secondary" onClick={this.handleClose}>
+                                          Close
+                                      </Button>
+                                      <Button variant="primary" onClick={this.updatesProfile}>
+                                          Save Changes
+                                      </Button>
+                                  </Modal.Footer>
+                              </Modal>
+                          </div>
+                      </div>
+
+                  </div>
+
+              </div>
+          </div>
+
+          </div>
+   </div>
+      )
+  }
 }
 
 const mapStateToProps = (state) => {
-    return {
-        profiledetails: state.profile.profiledetails,
-        imagePreviewresponse: state.image.imagePreviewresponse,
-        imageupload_url: state.image.imageupload_url
+  return {
+      profiledetails: state.profile.profiledetails,
+      imagePreviewresponse: state.image.imagePreviewresponse,
+      imageupload_url: state.image.imageupload_url,
+      isfollowingvalue: state.followingfollower.isfollowing,
+      followvalue: state.followingfollower.follow,
+      unfollowvalue: state.followingfollower.unfollow,
+      
+      follower_no: state.followingfollower.followerCount,
+        following_no: state.followingfollower.followingCount
 
-    };
+  };
 }
 export default connect(
-    mapStateToProps,
-    { profileaction, updateProfile, imageDownload, imageUpload }
+  mapStateToProps,
+  { profileaction, updateProfile, imageDownload, imageUpload, isfollowing, follow,followingCount, followerCount , unfollow}
 )(Profile);
